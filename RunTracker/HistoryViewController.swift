adde43a009627f9b5b65f93173c074f2f65d1dfd
Arseniy Zolotarev <aguznenkovv@yandex.ru>
2023-06-01 20:39:30 +0200

    Add gitignore & configure history vc

--- RunTracker/HistoryViewController.swift
+++ RunTracker/HistoryViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 final class HistoryViewController: UIViewController {
     // MARK: - UI Elements
+    private lazy var historyTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.dataSource = self
+        tableView.delegate = self
+        return tableView
+    }()
     
     // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         configureAppearance()
+        configureHistoryTableView()
     }
 }
 
@@ -23,6 +32,39 @@ private extension HistoryViewController {
         view.backgroundColor = .systemBackground
         title = "History"
         navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    func configureHistoryTableView() {
+        view.addSubview(historyTableView)
+        NSLayoutConstraint.activate([
+            historyTableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            historyTableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            historyTableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            historyTableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+}
+
+// MARK: - UITableViewDataSource
+extension HistoryViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        var content = cell.defaultContentConfiguration()
+        content.text = "history"
+        content.secondaryText = "secondary"
+        cell.contentConfiguration = content
+        return cell
+    }
+}
+
+// MARK: - UITableViewDelegate
+extension HistoryViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 }
 
